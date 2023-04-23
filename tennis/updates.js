64b140f160186a8cad46614e795692dce9becd98
raduka77 <radulazar@gmail.com>
2023-04-23 13:53:02 +0300

    a

--- tennis/updates.js
+++ tennis/updates.js
@@ -5,10 +5,12 @@ import async, { all } from 'async';
 import requests from 'sync-request';
 import { DateTime } from 'luxon';
 import slugify from 'slugify';
-import FetchPlayers from '../utils/tennis-utils.js';
-import makeCheckSlugsATP from '../utils/tennis-utils.js';
-import FetchMatches from '../utils/tennis-utils.js';
-import CreateDBFiles from '../utils/tennis-utils.js';
+import {
+  FetchPlayers,
+  makeCheckSlugsATP,
+  FetchMatches,
+  CreateDBFiles,
+} from '../utils/tennis-utils.js';
 
 await FetchPlayers();
 
