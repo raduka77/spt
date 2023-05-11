cd41ceccbf03731718e399c7eb3102f65ca27556
raduka77 <radulazar@gmail.com>
2023-05-11 17:08:51 +0300

    a

--- tennis/update-leagues.js
+++ tennis/update-leagues.js
@@ -11,9 +11,27 @@ import {
   CreateDBFiles,
 } from '../utils/tennis-leagues-utils.js';
 
-await FetchWTALeagues();
-await FetchATPLeagues();
-makeCheckSlugs();
-CreateDBFiles();
+const Start = async () => {
+  await FetchWTALeagues();
+  await FetchATPLeagues();
+  makeCheckSlugs();
+  CreateDBFiles();
 
-await MongoConnection.close();
+  await MongoConnection.close();
+};
+
+const RunUpdater = async ({ interval = 60, callback }) => {
+  console.log(`Started match updater with interval of `, interval, ` seconds`);
+  const MilisecondInterval = interval * 1000;
+
+  let timerId = setTimeout(async function tick() {
+    console.log(`Updating tennis leagues...`);
+    await Start();
+
+    timerId = setTimeout(tick, MilisecondInterval); // (*)
+  }, 1);
+};
+
+RunUpdater({
+  interval: 1, // in seconds - 3600 - 1 hour
+});
