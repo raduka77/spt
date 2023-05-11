11ff6c135c24131c01df03d679d62f73e8d1d8c7
raduka77 <radulazar@gmail.com>
2023-05-11 15:54:03 +0300

    a

--- utils/wta/handle-matches.js
+++ utils/wta/handle-matches.js
@@ -21,6 +21,9 @@ const handleNotStarted = async matchObj => {
   if (!fs.existsSync(matchFullPath)) {
     //// make dir if dir doesn't exist and write file
     console.log(`directory doesn't exist, making dir, writing file`);
+    console.log(
+      `Id: ${matchObj.match.id}, dir: ${matchFullPath}/${matchObj.awayPlayerData.id}.json`
+    );
     fs.mkdirSync(matchFullPath, { recursive: true });
 
     //// write db file
@@ -32,7 +35,9 @@ const handleNotStarted = async matchObj => {
   } else {
     /// just write file
     console.log(`directory EXIST, just writing file`);
-
+    console.log(
+      `Id: ${matchObj.match.id}, dir: ${matchFullPath}/${matchObj.awayPlayerData.id}.json`
+    );
     //// write db file
     fs.writeFileSync(
       `${matchFullPath}/${matchObj.awayPlayerData.id}.json`,
